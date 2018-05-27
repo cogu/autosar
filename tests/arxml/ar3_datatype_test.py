765d859218b45ea54e3926905c533dedecede9d4
cogu <congus8@gmail.com>
2018-05-27 23:44:14 +0200

    Updated unit tests for XML generation

--- tests/arxml/ar3_datatype_test.py
+++ tests/arxml/ar3_datatype_test.py
@@ -12,8 +12,6 @@ def _create_packages(ws):
     package=ws.createPackage('DataType', role='DataType')
     package.createSubPackage('CompuMethod', role='CompuMethod')    
     package.createSubPackage('Units', role='Unit')
-    ws.createPackage('Constant', role='Constant')
-    _create_base_types(ws)
 
 def _create_base_types(ws):
     package = ws.find('/DataType')
@@ -49,52 +47,55 @@ class TestDataTypeARXML3(unittest.TestCase):
         self.assertEqual(expected_text, generated_text)
         os.remove(generated_path)
 
-    def test_create_integer_constant(self):
+    def test_create_integer_types(self):
         ws = autosar.workspace(version="3.0.2")
         _create_packages(ws)
         package = ws.find('/DataType')
-        package.createIntegerDataType('Warning3Bit_T', min=0, max=7)
-        package = ws.find('/Constant')
-        package.createConstant('WarningSignal_IV', 'Warning3Bit_T', 7)
-        
-        file_name = 'ar3_integer_constant.arxml'
+        package.createIntegerDataType('UInt8', min=0, max=255)
+        package.createIntegerDataType('UInt16', min=0, max=65535)
+        package.createIntegerDataType('UInt32', min=0, max=4294967295)
+        file_name = 'ar3_integer_types.arxml'
         generated_file = os.path.join(_output_dir, file_name)
-        expected_file = os.path.join( 'expected_gen', 'constant', file_name)
-        self.save_and_check(ws, expected_file, generated_file, ['/Constant'])
-
-    def test_create_array_record_constant(self):
+        expected_file = os.path.join( 'expected_gen', 'datatype', file_name)
+        self.save_and_check(ws, expected_file, generated_file, ['/DataType'])
+   
+    def test_create_record_type_simple(self):
         ws = autosar.workspace(version="3.0.2")
         _create_packages(ws)
+        _create_base_types(ws)
+        package = ws.find('/DataType')
+        
+        package.createRecordDataType('MyRecord_T',
+                             elements = [
+                                ('UserId1', 'UInt16'),
+                                ('UserId2', 'UInt16'),
+                                ('UserId3', 'UInt16')
+                                ])
+        file_name = 'ar3_record_type_simple.arxml'
+        generated_file = os.path.join(_output_dir, file_name)
+        expected_file = os.path.join( 'expected_gen', 'datatype', file_name)
+        self.save_and_check(ws, expected_file, generated_file, ['/DataType'])
+
+    def test_create_record_type_array(self):
+        ws = autosar.workspace(version="3.0.2")
+        _create_packages(ws)
+        _create_base_types(ws)
         package = ws.find('/DataType')        
         record_type = package.createRecordDataType('MyRecord_T',
-                                                    elements = [
-                                                    ('Setting1', 'UInt8'),
-                                                    ('Setting2', 'UInt16'),
-                                                    ('Setting3', 'UInt32')
-                                                    ])
-        package.createArrayDataType('MyRecordArray_T', record_type.ref, 3)
-        
-        package = ws.find('/Constant')
-        package.createConstant('MyConstant', 'MyRecordArray_T', [
-                                                                    {'Setting1': 255, 'Setting2': 65535, 'Setting3': 2**32-1},
-                                                                    {'Setting1': 0, 'Setting2': 0, 'Setting3': 0},
-                                                                    {'Setting1': 255, 'Setting2': 65535, 'Setting3': 2**32-1}
-                                                                ])
-        
-        file_name = 'ar3_array_record_constant.arxml'
+                             elements = [
+                                ('Setting1', 'UInt8'),
+                                ('Setting2', 'UInt16'),
+                                ('Setting3', 'UInt32')
+                                ])
+        package.createArrayDataType('MyArray_T', record_type.ref, 8)
+        file_name = 'ar3_record_type_array.arxml'
         generated_file = os.path.join(_output_dir, file_name)
-        expected_file = os.path.join( 'expected_gen', 'constant', file_name)
-        self.save_and_check(ws, expected_file, generated_file, ['/Constant'], True)
-   
-    # def test_create_record_type_array(self):
-    #     ws = autosar.workspace(version="3.0.2")
-    #     _create_packages(ws)
-    #     _create_base_types(ws)
-    #     package = ws.find('/DataType')        
-    #     file_name = 'ar3_record_type_array.arxml'
-    #     generated_file = os.path.join(_output_dir, file_name)
-    #     expected_file = os.path.join( 'expected_gen', 'datatype', file_name)
-    #     self.save_and_check(ws, expected_file, generated_file, ['/DataType'], True)
+        expected_file = os.path.join( 'expected_gen', 'datatype', file_name)
+        self.save_and_check(ws, expected_file, generated_file, ['/DataType'], True)
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
