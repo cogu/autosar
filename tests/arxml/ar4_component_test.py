8d741a11a98c5be5b2dc17ad9416455fc78c01d9
cogu <congus8@gmail.com>
2018-05-28 15:45:36 +0200

    Unit Test refactoring

--- tests/arxml/ar4_component_test.py
+++ tests/arxml/ar4_component_test.py
@@ -1,11 +1,8 @@
 import os, sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 import autosar
+from tests.arxml.common import ARXMLTestClass
 import unittest
-import time
-import shutil
-
-_output_dir = 'derived'
 
 def _create_packages(ws):
 
@@ -39,19 +36,8 @@ def _create_packages(ws):
 
     package = ws.createPackage('ComponentTypes', role='ComponentType')
 
-class TestComponentARXML4(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        output_dir_full = os.path.join(os.path.dirname(__file__), _output_dir)
-        if not os.path.exists(output_dir_full):
-            os.makedirs(output_dir_full)
-            time.sleep(0.1)
-
-    @classmethod
-    def tearDownClass(cls):
-        os.rmdir(_output_dir)
-
+class ARXML4ComponentTest(ARXMLTestClass):
+    
     def test_create_application_software_component(self):
         ws = autosar.workspace(version="4.2.2")
         _create_packages(ws)
@@ -61,15 +47,10 @@ class TestComponentARXML4(unittest.TestCase):
         swc.createRequirePort('FreeRunningTimer', 'FreeRunningTimer5ms_I')
         swc.behavior.createRunnable('Run', portAccess=['VehicleSpeed', 'FreeRunningTimer/GetTime', 'FreeRunningTimer/IsTimerElapsed'])
         swc.behavior.createTimerEvent('Run', 20) #execute the Run function every 20ms in all modes
-        generated_file = os.path.join(_output_dir, 'ar4_application_swc.arxml')
-        expected_file = os.path.join( 'expected_gen', 'component', 'ar4_application_swc.arxml')
-        ws.saveXML(generated_file, filters=['/ComponentTypes'])
-        with open (generated_file, "r") as fp:
-            generated_text=fp.read()
-        with open (expected_file, "r") as fp:
-            expected_text=fp.read()
-        self.assertEqual(generated_text, expected_text)
-        os.remove(generated_file)
+        file_name = 'ar4_application_swc.arxml'
+        generated_file = os.path.join(self.output_dir, file_name)
+        expected_file = os.path.join( 'expected_gen', 'constant', file_name)
+        self.save_and_check(ws, expected_file, generated_file, ['/ComponentTypes'])
 
     def test_create_service_software_component(self):
         ws = autosar.workspace(version="4.2.2")
@@ -77,15 +58,10 @@ class TestComponentARXML4(unittest.TestCase):
         package = ws.find('/ComponentTypes')
         swc = package.createServiceComponent('MyService')
         swc.createRequirePort('VehicleSpeed', 'VehicleSpeed_I', initValueRef = 'VehicleSpeed_IV')
-        generated_file = os.path.join(_output_dir, 'ar4_service_swc.arxml')
-        expected_file = os.path.join( 'expected_gen', 'component', 'ar4_service_swc.arxml')
-        ws.saveXML(generated_file, filters=['/ComponentTypes'])
-        with open (generated_file, "r") as fp:
-            generated_text=fp.read()
-        with open (expected_file, "r") as fp:
-            expected_text=fp.read()
-        self.assertEqual(generated_text, expected_text)
-        os.remove(generated_file)
+        file_name = 'ar4_service_swc.arxml'
+        generated_file = os.path.join(self.output_dir, file_name)
+        expected_file = os.path.join( 'expected_gen', 'constant', file_name)
+        self.save_and_check(ws, expected_file, generated_file, ['/ComponentTypes'])        
 
     def test_create_cdd_software_component(self):
         ws = autosar.workspace(version="4.2.2")
@@ -93,16 +69,10 @@ class TestComponentARXML4(unittest.TestCase):
         package = ws.find('/ComponentTypes')
         swc = package.createComplexDeviceDriverComponent('MyService')
         swc.createRequirePort('VehicleSpeed', 'VehicleSpeed_I', initValueRef = 'VehicleSpeed_IV')
-        generated_file = os.path.join(_output_dir, 'ar4_cdd_swc.arxml')
-        expected_file = os.path.join( 'expected_gen', 'component', 'ar4_cdd_swc.arxml')
-        ws.saveXML(generated_file, filters=['/ComponentTypes'])
-        with open (generated_file, "r") as fp:
-            generated_text=fp.read()
-        with open (expected_file, "r") as fp:
-            expected_text=fp.read()
-        self.assertEqual(generated_text, expected_text)
-        os.remove(generated_file)
-
+        file_name = 'ar4_cdd_swc.arxml'
+        generated_file = os.path.join(self.output_dir, file_name)
+        expected_file = os.path.join( 'expected_gen', 'constant', file_name)
+        self.save_and_check(ws, expected_file, generated_file, ['/ComponentTypes'])          
 
 if __name__ == '__main__':
     unittest.main()
