65e9d03562ee1f9f7aae380b3ebad958378a31b8
cogu <congus8@gmail.com>
2019-11-01 14:18:16 +0100

    Add support for provide port mode access
    Upgrade behavior_parser from old to new code style

--- tests/arxml/ar4_behavior_test.py
+++ tests/arxml/ar4_behavior_test.py
@@ -50,39 +50,108 @@ def _create_port_interfaces(ws):
     portInterface['IsTimerElapsed'].createInArgument('startTime', 'uint32')
     portInterface['IsTimerElapsed'].createInArgument('duration', 'uint32')
     portInterface['IsTimerElapsed'].createOutArgument('result', 'boolean')
+    package.createModeSwitchInterface('VehicleMode_I', autosar.mode.ModeGroup('mode', 'VehicleMode'))
 
 def _create_constants(ws):
     package = ws.find('/Constants')
     package.createConstant('VehicleSpeed_IV', 'VehicleSpeed_T', 65535)
     package.createConstant('EngineSpeed_IV', 'EngineSpeed_T', 65535)
-    
-def _create_components(ws):
-    package = ws.find('/ComponentTypes')
-    swc = package.createApplicationSoftwareComponent('GaugeHandler')
-    swc.createRequirePort('VehicleSpeed', '/PortInterfaces/VehicleSpeed_I', initValueRef='/Constants/VehicleSpeed_IV')
-    swc.createRequirePort('EngineSpeed', '/PortInterfaces/EngineSpeed_I', initValueRef='/Constants/EngineSpeed_IV')
+
+
+def _create_mode_declarations(ws):
+    package = ws.find('ModeDclrGroups')
+    package.createModeDeclarationGroup('VehicleMode', ["OFF",
+                                                       "ACCESSORY",
+                                                        "RUNNING",
+                                                        "CRANKING",
+                                                    ], "OFF")
 
 def _init_ws():
     ws = autosar.workspace('4.2.2')
     _create_packages(ws)
     _create_data_types(ws)
+    _create_mode_declarations(ws)
     _create_port_interfaces(ws)
     _create_constants(ws)
-    _create_components(ws)
+
     return ws
 
 class ARXML4BehaviorTest(ARXMLTestClass):
-    
+
     def test_create_empty_behavior_from_swc(self):
+
         ws = _init_ws()
+        swc1 = ws['ComponentTypes'].createApplicationSoftwareComponent('MyApplication')
         file_name = 'ar4_behavior_empty_from_swc.arxml'
         generated_file = os.path.join(self.output_dir, file_name)
         expected_file = os.path.join( 'expected_gen', 'behavior', file_name)
         self.save_and_check(ws, expected_file, generated_file, ['/ComponentTypes'])
-        
-        
+
+        ws2 = autosar.workspace(ws.version_str)
+        ws2.loadXML(os.path.join(os.path.dirname(__file__), expected_file))
+        swc2 = ws2.find(swc1.ref)
+        self.assertIsInstance(swc2, autosar.component.ApplicationSoftwareComponent)
+
+    def test_create_runnable_with_init_event(self):
+
+        ws = _init_ws()
+        swc1 = ws['ComponentTypes'].createApplicationSoftwareComponent('MyApplication')
+        swc1.behavior.createRunnable('MyApplication_Init')
+        swc1.behavior.createInitEvent('MyApplication_Init')
+
+        file_name = 'ar4_runnable_with_init_event.arxml'
+        generated_file = os.path.join(self.output_dir, file_name)
+        expected_file = os.path.join( 'expected_gen', 'behavior', file_name)
+        self.save_and_check(ws, expected_file, generated_file, ['/ComponentTypes'])
+
+        ws2 = autosar.workspace(ws.version_str)
+        ws2.loadXML(os.path.join(os.path.dirname(__file__), expected_file))
+        swc2 = ws2.find(swc1.ref)
+        self.assertIsInstance(swc2, autosar.component.ApplicationSoftwareComponent)
+
+    def test_create_runnable_with_require_type_mode_switch_trigger(self):
+
+        ws = _init_ws()
+        swc1 = ws['ComponentTypes'].createApplicationSoftwareComponent('MyApplication')
+        swc1.createRequirePort('VehicleMode', '/PortInterfaces/VehicleMode_I')
+        swc1.behavior.createRunnable('MyApplication_Init')
+        swc1.behavior.createModeSwitchEvent('MyApplication_Init', 'VehicleMode/ACCESSORY', activationType='ENTRY')
+
+        file_name = 'ar4_runnable_with_require_type_mode_switch_trigger.arxml'
+        generated_file = os.path.join(self.output_dir, file_name)
+        expected_file = os.path.join( 'expected_gen', 'behavior', file_name)
+        self.save_and_check(ws, expected_file, generated_file, ['/ComponentTypes'])
+
+        ws2 = autosar.workspace(ws.version_str)
+        ws2.loadXML(os.path.join(os.path.dirname(__file__), expected_file))
+        swc2 = ws2.find(swc1.ref)
+        self.assertIsInstance(swc2, autosar.component.ApplicationSoftwareComponent)
+
+    def test_create_runnable_with_provide_type_mode_access(self):
+
+        ws = _init_ws()
+        swc1 = ws['ComponentTypes'].createApplicationSoftwareComponent('MyApplication')
+        port1 = swc1.createProvidePort('VehicleMode', '/PortInterfaces/VehicleMode_I', queueLength=1, modeSwitchAckTimeout=10)
+        runnable1 = swc1.behavior.createRunnable('MyApplication_Init', portAccess=['VehicleMode'])
+
+        file_name = 'ar4_runnable_with_provide_type_mode_access.arxml'
+        generated_file = os.path.join(self.output_dir, file_name)
+        expected_file = os.path.join( 'expected_gen', 'behavior', file_name)
+        self.save_and_check(ws, expected_file, generated_file, ['/ComponentTypes'])
+
+        ws2 = autosar.workspace(ws.version_str)
+        ws2.loadXML(os.path.join(os.path.dirname(__file__), expected_file))
+        swc2 = ws2.find(swc1.ref)
+        self.assertIsInstance(swc2, autosar.component.ApplicationSoftwareComponent)
+        port2 = ws2.find(port1.ref)
+        self.assertIsInstance(port2, autosar.component.ProvidePort)
+        runnable2 = swc2.behavior.find(runnable1.name)
+        self.assertIsInstance(runnable2, autosar.behavior.RunnableEntity)
+        modeAccessPoint = runnable2.modeAccessPoints[0]
+        self.assertEqual(modeAccessPoint.modeGroupRef, '/PortInterfaces/VehicleMode_I/mode')
+        self.assertEqual(modeAccessPoint.providePortRef, '/ComponentTypes/MyApplication/VehicleMode')
 
 
 if __name__ == '__main__':
+
     unittest.main()
-    
