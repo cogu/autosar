1f4e3409104452909410e11f913f5227da380124
cogu <congus8@gmail.com>
2024-03-30 12:08:46 +0100

    Change to markdown file in setup config

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 
 def readme():
-    with open('README.rst') as f:
+    with open('README.md') as f:
         return f.read()
 
 setup(name='autosar',
       version='0.4.2',
       description='A set of Python modules for working with AUTOSAR XML files',
       long_description=readme(),
-      long_description_content_type='text/x-rst',
+      long_description_content_type='text/markdown',
       classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
