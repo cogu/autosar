682c59c5c0d1466868ed58b9076f15444b60f667
cogu <congus8@gmail.com>
2024-09-16 23:16:30 +0200

    Add event creation API
    - New event API added to SwcInternalBehavior
    - Various fixes and improvements

--- src/autosar/base.py
+++ src/autosar/base.py
@@ -6,10 +6,21 @@ DEFAULT_SCHEMA_VERSION = 51
 
 
 def split_ref(ref: str) -> list[str] | None:
-    """Splits an autosar reference string into a list of strings"""
+    """Splits a string separated with slashes and automatially removes leading slash"""
+    sep = "/"
     if isinstance(ref, str):
-        return ref[1:].split('/') if ref[0] == '/' else ref.split('/')
+        return ref[1:].split(sep) if ref[0] == sep else ref.split(sep)
     return None
+
+
+def split_ref_strict(ref: str) -> list[str] | None:
+    """Splits a strings separated with slashes but throws an error if the string starts with a slash"""
+    sep = "/"
+    if not isinstance(ref, str):
+        raise TypeError("ref: Must be a string")
+    if ref[0] == sep:
+        raise ValueError("ref: String cannot start with '/'")
+    return ref.split(sep)
 
 
 class Searchable(abc.ABC):
