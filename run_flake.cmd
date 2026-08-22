88d3ae9e4abb541d9ee3115e298f3b6cdba106d6
cogu <congus8@gmail.com>
2026-08-22 18:20:51 +0200

    Add Python 12 and 13 to the action workflow
    * Update GitHub workflow file
    * Fix flake8 ignore rules

--- run_flake.cmd
+++ run_flake.cmd
@@ -1,5 +1,5 @@
 @echo off
-flake8 --max-line-length=120 --ignore=D107,D200,D205,D400,D401,W503 src
-flake8 --max-line-length=120 --ignore=D101,D102,D107,D200,D205,D400,D401,E402,W503 tests
-flake8 --max-line-length=120 --ignore=D107,D200,D205,D400,D401,W503 examples
-flake8 --max-line-length=120 --ignore=D107,D200,D205,D400,D401,W503 dev_utils
+flake8 --max-line-length=120 --extend-ignore=D107,D200,D205,D400,D401 src
+flake8 --max-line-length=120 --extend-ignore=D101,D102,D107,D200,D205,D400,D401,E402 tests
+flake8 --max-line-length=120 --extend-ignore=D107,D200,D205,D400,D401 examples
+flake8 --max-line-length=120 --extend-ignore=D107,D200,D205,D400,D401 dev_utils
